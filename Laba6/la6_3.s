93474458059c5d7352b10128cd4e9e565486f46b
Yulia <60868878+yulia9112@users.noreply.github.com>
2020-12-20 12:21:33 +0300

    Add files via upload

--- Laba6/la6_3.s
+++ Laba6/la6_3.s
@@ -1,58 +1,58 @@
 .data
 input: .string "%d"
 output: .string "a[%d]=%d\n"
-inputN: .string "Введите N: "
-inputX: .string "Введите X: "
+inputN: .string "Enter N: "
+inputX: .string "Enter X: "
 x:
-.int 0
+    .int 0
 n:
-.int 0
+    .int 0
 i:
-.int -1
+    .int -1
 a:
-.int 0
-.globl main
-main:
-pushl %ebp
-movl %esp, %ebp
-subl $40, %esp
-movl $inputX, (%esp)
-call printf
-leal 36(%esp), %eax
-movl %eax, 4(%esp)
-movl $input, (%esp)
-call scanf
-movl -4(%ebp), %eax
-movl %eax, x
-movl %ebp, %esp
-popl %ebp
-pushl %ebp
-movl %esp, %ebp
-subl $40, %esp
-movl $inputN, (%esp)
-call printf
-leal 36(%esp), %eax
-movl %eax, 4(%esp)
-movl $input, (%esp)
-call scanf
-movl -4(%ebp), %eax
-movl %eax, n
-movl %ebp, %esp
-popl %ebp
-begin_iteration:
-cmpl $0,n
-je end_loop
-addl $-1, n
-addl $1, i
-end_iteration:
-push a
-push a
-push i
-push $output
-call printf
-addl $16, %esp
-movl x, %eax
-addl %eax, a
-jmp begin_iteration
-end_loop:
-ret
+    .int 0
+.globl _main
+_main:
+    pushl %ebp
+    movl %esp, %ebp
+    subl $40, %esp
+    movl $inputX, (%esp)
+    call _printf
+    leal 36(%esp), %eax
+    movl %eax, 4(%esp)
+    movl $input, (%esp)
+    call _scanf
+    movl -4(%ebp), %eax
+    movl %eax, x
+    movl %ebp, %esp
+    popl %ebp
+    pushl %ebp
+    movl %esp, %ebp
+    subl $40, %esp
+    movl $inputN, (%esp)
+    call _printf
+    leal 36(%esp), %eax
+    movl %eax, 4(%esp)
+    movl $input, (%esp)
+    call _scanf
+    movl -4(%ebp), %eax
+    movl %eax, n
+    movl %ebp, %esp
+    popl %ebp
+_begin_iteration:
+    cmpl $0,n
+    je _end_loop
+    addl $-1, n
+    addl $1, i
+_end_iteration:
+    push a
+    push a
+    push i
+    push $output
+    call _printf
+    addl $16, %esp
+    movl x, %eax
+    addl %eax, a
+    jmp _begin_iteration
+_end_loop:
+    ret
